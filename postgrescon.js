476ce9a3c6ba21fd1dbd6fc2b6720220565a5cff
kimjulia1117 <kimjulia1117@gmail.com>
2020-03-03 18:47:50 -0600

    Change to mints database

--- postgrescon.js
+++ postgrescon.js
@@ -1,4 +1,4 @@
-const PSQL_LOGIN = "postgres://jbk170030:teamlary@localhost:5442/postgres"
+const PSQL_LOGIN = "postgres://jbk170030:teamlary@localhost:5442/mints"
 
 module.exports = {
 	PSQL_LOGIN
